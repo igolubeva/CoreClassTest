f48c5d87e579e32c4f6a97934eb5f037f8fd9d6b
Irina Golubeva <irinadanshina@mail.ru>
2019-06-26 22:54:58 +0300

    fix webpack and now primereact working ok

--- data/webpack.config.js
+++ data/webpack.config.js
@@ -27,7 +27,33 @@ module.exports = {
 			},
 			{
 				test: /\.(sa|sc|c)ss$/,
-				use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"]
+				use: ["style-loader", "css-loader", "sass-loader", "postcss-loader",  "resolve-url-loader"]
+			},
+			{
+				test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=asset_[hash].[ext]',
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'file-loader?name=asset_[hash].[ext]',
+			},
+			{ test: /\.png$/, loader: 'file-loader?name=asset_[hash].[ext]' },
+			{
+				include: /\.json$/,
+				loaders: ['json-loader'],
+			},
+			{
+				test: /\.gif$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true, // webpack@1.x
+							disable: true, // webpack@2.x and newer
+						},
+					},
+				],
 			}
 		]
 	}
